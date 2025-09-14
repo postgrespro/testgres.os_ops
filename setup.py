6d91232f90d9346c5e3eca09455ba65c037c9929
d.kovalenko <d.kovalenko@postgrespro.ru>
2025-09-14 21:16:06 +0300

    setup.py is updated (psutil)

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ try:
 except ImportError:
     from distutils.core import setup
 
+# Basic dependencies
 install_requires = [
+    "psutil",
     "testgres.common @ git+https://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@github.com/postgrespro/testgres.common.git",
 ]
 
