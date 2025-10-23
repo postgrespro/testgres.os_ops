cd94551fcd9ac3ac98f3c3758241fe45016c437d
vshepard <v.shepard@postgrespro.ru>
2025-10-23 12:59:13 +0200

    Prepare for pulish to PyPi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 install_requires = [
     "psutil",
     "six>=1.9.0",
-    "testgres.common @ git+https://github.com/postgrespro/testgres.common.git",
+    "testgres.common==0.0.1",
 ]
 
 setup(
