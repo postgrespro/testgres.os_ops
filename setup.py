4bbbcd141a383d5a010aa69a84da28005f1d3fcb
d.kovalenko <d.kovalenko@postgrespro.ru>
2025-11-01 15:48:01 +0300

    setup.py is updated (readme)

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ try:
 except ImportError:
     from distutils.core import setup
 
+# Get contents of README file
+with open("README.md", "r") as f:
+    readme = f.read()
+
 # Basic dependencies
 install_requires = [
     "psutil",
@@ -19,6 +23,7 @@ setup(
     package_dir={"testgres.operations": "src"},
     description='Testgres subsystem to work with OS',
     url='https://github.com/postgrespro/testgres.os_ops',
+    long_description=readme,
     long_description_content_type='text/markdown',
     license='PostgreSQL',
     author='Postgres Professional',
