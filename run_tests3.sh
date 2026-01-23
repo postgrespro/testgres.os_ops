9f915d733ac0905ddd75002ccf83b9609ca0c430
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-01-23 18:45:34 +0300

    [CI] AltLinux 10/11 are tested

--- run_tests3.sh
+++ run_tests3.sh
@@ -5,16 +5,15 @@ set -eux
 # prepare python environment
 VENV_PATH="/tmp/testgres_venv"
 rm -rf $VENV_PATH
-python -m venv "${VENV_PATH}"
+python3 -m venv "${VENV_PATH}"
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 source "${VENV_PATH}/bin/activate"
-pip install -r tests/requirements.txt
+python3 -m pip install -r tests/requirements.txt
 
 # check code style
 flake8 .
 
 # run builtin tests
-python -m pytest -l -vvv -n 4
+python3 -m pytest -l -vvv -n 4
 
 set +eux
-
