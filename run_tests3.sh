41de0ba7b485072eed8dd8dd2390ad86355fd3ea
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-01-27 15:13:20 +0300

    Version 2.0.2 (min python is 3.7.3)
    run_tests3.sh does not run flake8.

--- run_tests3.sh
+++ run_tests3.sh
@@ -10,9 +10,6 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 source "${VENV_PATH}/bin/activate"
 python3 -m pip install -r tests/requirements.txt
 
-# check code style
-flake8 .
-
 # run builtin tests
 python3 -m pytest -l -vvv -n 4
 
