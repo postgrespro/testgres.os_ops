f2e5ed7833a2243afac650d81d55121181a96b09
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-07-02 20:24:35 +0300

    rmt: LocalCheck is updated
    New methods:
     - check_isdir
     - check_not_isdir
     - check_isfile
     - check_not_isfile

--- tests/helpers/local_check.py
+++ tests/helpers/local_check.py
@@ -44,3 +44,83 @@ class LocalCheck:
             path,
         )
         raise RuntimeError(err_msg)
+
+    # --------------------------------------------------------------------
+    @staticmethod
+    def check_isdir(
+        os_ops: OsOperations,
+        path: str,
+    ) -> None:
+        assert isinstance(os_ops, OsOperations)
+        assert type(path) is str
+
+        if not OsOpsHelpers.is_localhost(os_ops):
+            return
+
+        if os.path.isdir(path):
+            return
+
+        err_msg = "[LocalCheck] Local path [{}] is not dir.".format(
+            path,
+        )
+        raise RuntimeError(err_msg)
+
+    # --------------------------------------------------------------------
+    @staticmethod
+    def check_not_isdir(
+        os_ops: OsOperations,
+        path: str,
+    ) -> None:
+        assert isinstance(os_ops, OsOperations)
+        assert type(path) is str
+
+        if not OsOpsHelpers.is_localhost(os_ops):
+            return
+
+        if not os.path.isdir(path):
+            return
+
+        err_msg = "[LocalCheck] Local path [{}] is dir.".format(
+            path,
+        )
+        raise RuntimeError(err_msg)
+
+    # --------------------------------------------------------------------
+    @staticmethod
+    def check_isfile(
+        os_ops: OsOperations,
+        path: str,
+    ) -> None:
+        assert isinstance(os_ops, OsOperations)
+        assert type(path) is str
+
+        if not OsOpsHelpers.is_localhost(os_ops):
+            return
+
+        if os.path.isfile(path):
+            return
+
+        err_msg = "[LocalCheck] Local path [{}] is not file.".format(
+            path,
+        )
+        raise RuntimeError(err_msg)
+
+    # --------------------------------------------------------------------
+    @staticmethod
+    def check_not_isfile(
+        os_ops: OsOperations,
+        path: str,
+    ) -> None:
+        assert isinstance(os_ops, OsOperations)
+        assert type(path) is str
+
+        if not OsOpsHelpers.is_localhost(os_ops):
+            return
+
+        if not os.path.isfile(path):
+            return
+
+        err_msg = "[LocalCheck] Local path [{}] is file.".format(
+            path,
+        )
+        raise RuntimeError(err_msg)
