ad691b57a71c25a19a96f7b0a261a7fe234c94c2
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-07-02 20:25:01 +0300

    rmt: test_isfile_false__directory is updated

--- tests/test_os_ops_common.py
+++ tests/test_os_ops_common.py
@@ -1128,12 +1128,14 @@ class TestOsOpsCommon:
         os_ops = os_ops_descr.os_ops
         assert isinstance(os_ops, OsOperations)
 
-        name = os.path.dirname(__file__)
+        tmpdir = os_ops.get_tempdir()
+        LocalCheck.check_path_exists(os_ops, tmpdir)
+        LocalCheck.check_isdir(os_ops, tmpdir)
+        LocalCheck.check_not_isfile(os_ops, tmpdir)
+        assert os_ops.path_exists(tmpdir)
+        assert os_ops.isdir(tmpdir)
 
-        assert os_ops.isdir(name)
-
-        response = os_ops.isfile(name)
-
+        response = os_ops.isfile(tmpdir)
         assert response is False
         return
 
