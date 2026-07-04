f962fece5292e6d70cbf9ae7b378df837e5d32f4
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-07-04 18:58:42 +0300

    OsOpsDescrs is updated
    - it uses _to_int_or_none
    - it os.getenv('TEST_CFG__REMOTE_HOST', '127.0.0.1')

--- tests/helpers/global_data.py
+++ tests/helpers/global_data.py
@@ -4,6 +4,7 @@ from src.local_ops import LocalOperations
 from src.remote_ops import RemoteOperations
 
 import os
+import typing
 
 
 class OsOpsDescr:
@@ -17,10 +18,14 @@ class OsOpsDescr:
         self.os_ops = os_ops
 
 
+def _to_int_or_none(v: typing.Any) -> typing.Optional[int]:
+    return v if v is None else int(v)
+
+
 class OsOpsDescrs:
     sm_remote_conn_params = ConnectionParams(
-        host=os.getenv('TEST_CFG__REMOTE_HOST') or '127.0.0.1',
-        port=int(os.getenv('TEST_CFG__REMOTE_PORT', 0)) if os.getenv('TEST_CFG__REMOTE_PORT') else None,
+        host=os.getenv('TEST_CFG__REMOTE_HOST', '127.0.0.1'),
+        port=_to_int_or_none(os.getenv('TEST_CFG__REMOTE_PORT')),
         username=os.getenv('TEST_CFG__REMOTE_USERNAME'),
         ssh_key=os.getenv('TEST_CFG__REMOTE_SSH_KEY'),
         password=os.getenv('TEST_CFG__REMOTE_PASSWORD'),
