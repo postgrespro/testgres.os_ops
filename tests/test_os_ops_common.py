81117d783b83faba65cc7b784ae1ecba4f105a70
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-09-14 11:37:43 +0300

    fix: kill/terminate on alpine via ssh returns 255 instead 143/137

--- tests/test_os_ops_common.py
+++ tests/test_os_ops_common.py
@@ -4342,7 +4342,8 @@ print('b', file=sys.stderr)
             if type(os_ops).__name__ == "LocalOperations":
                 assert rc == -15
             elif type(os_ops).__name__ == "RemoteOperations":
-                assert rc == 143
+                # Alpine returns 255
+                assert rc in [143, 255]
             else:
                 raise RuntimeError("Unknown os_ops type: {}".format(
                     type(os_ops).__name__,
@@ -4378,7 +4379,8 @@ print('b', file=sys.stderr)
             if type(os_ops).__name__ == "LocalOperations":
                 assert rc == -9
             elif type(os_ops).__name__ == "RemoteOperations":
-                assert rc == 137
+                # Alpine returns 255
+                assert rc in [137, 255]
             else:
                 raise RuntimeError("Unknown os_ops type: {}".format(
                     type(os_ops).__name__,
