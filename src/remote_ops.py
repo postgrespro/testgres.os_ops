d1384a68c23a3182e2b98a75d7e681bb0f229934
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-09-16 16:48:45 +0300

    remote-tt: RemoteOperations::mkdtemp is updated

--- src/remote_ops.py
+++ src/remote_ops.py
@@ -1192,26 +1192,28 @@ class RemoteOperations(OsOperations):
 
         command = " ".join(command_p)
 
-        exec_r = self.exec_command(command, verbose=True, encoding=get_default_encoding(), ignore_errors=True)
+        exec_r = self._transport_run(
+            command,
+            encoding=get_default_encoding(),
+            check=False,
+        )
 
-        assert type(exec_r) is tuple
-        assert len(exec_r) == 3
+        assert type(exec_r) is __class__.tagTransportRunResult
 
-        exec_exitcode, exec_output, exec_error = exec_r
+        assert type(exec_r.returncode) is int
+        assert type(exec_r.stdout) is str
+        assert type(exec_r.stderr) is str
 
-        assert type(exec_exitcode) is int
-        assert type(exec_output) is str
-        assert type(exec_error) is str
-
-        if exec_exitcode != 0:
+        if exec_r.returncode != 0:
             RaiseError.CommandExecutionError(
                 cmd=command,
-                exit_code=exec_exitcode,
+                exit_code=exec_r.returncode,
                 message="Could not create temporary directory.",
-                error=exec_error,
-                out=exec_output)
+                error=exec_r.stderr,
+                out=exec_r.stdout,
+            )
 
-        temp_dir = exec_output.strip()
+        temp_dir = exec_r.stdout.strip()
         return temp_dir
 
     def mkstemp(self, prefix: typing.Optional[str] = None) -> str:
