fb31aca45d3d980e7842e57920f296a76f52ab46
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-09-22 00:00:56 +0300

    macos: run local tests only

--- run_tests3.sh
+++ run_tests3.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eux
 
+if [ -z ${TEST_FILTER+x} ]; \
+then export TEST_FILTER=""; \
+fi
+
 # prepare python environment
 VENV_PATH="/tmp/testgres_venv"
 rm -rf $VENV_PATH
@@ -13,6 +17,6 @@ pip install -r tests/requirements.txt
 export -p
 
 # run builtin tests
-pytest -l -vvv -n 4
+pytest -l -vvv -n 4 -k "${TEST_FILTER}"
 
 set +eux
